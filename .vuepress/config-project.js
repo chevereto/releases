73f4a9ad0bc6cdd64f6251b6f579361def4dbb7b
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-06-16 11:26:55 -0400

    vuepress

---
+++ .vuepress/config-project.js
@@ -0,0 +1,64 @@
+module.exports = {
+    title: 'Releases',
+    description: 'Chevereto Releases',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/logo.svg`
+        }],
+        ['link', {
+            rel: 'manifest',
+            href: '/manifest.json'
+        }],
+        ['meta', {
+            name: 'theme-color',
+            content: '#23a8e0'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'black'
+        }],
+        ['link', {
+            rel: 'apple-touch-icon',
+            href: '/logo.svg'
+        }],
+        ['link', {
+            rel: 'mask-icon',
+            href: '/logo.svg',
+            color: '#3eaf7c'
+        }],
+        ['meta', {
+            name: 'msapplication-TileImage',
+            content: '/logo.svg'
+        }],
+        ['meta', {
+            name: 'msapplication-TileColor',
+            content: '#000000'
+        }]
+    ],
+    themeConfig: {
+        logo: '/logo.svg',
+        // repo: 'chevereto/chevereto',
+        docsRepo: 'chevereto/releases',
+        docsBranch: 'main',
+        smoothScroll: true,
+        editLinks: false,
+        lastUpdated: true,
+        // algolia: {
+        //     apiKey: '250992199b7863078c8399e2638107af',
+        //     indexName: 'chevereto',
+        // }
+    },
+    plugins: [
+        // [
+        //     '@vuepress/google-analytics',
+        //     {
+        //     'ga': 'UA-25384365-3'
+        //     }
+        // ]
+    ]
+};
